6cc3375eed07b63224a505e2c943000e383970c4
Fredhack1 <mirdo2co@gmail.com>
2025-08-05 14:41:16 +0100

    Initial commit: backend Spring Boot setup

---
+++ backend/src/main/java/com/taskito_web/backend/model/Task.java
@@ -0,0 +1,77 @@
+package com.taskito_web.backend.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private String status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime dueAt;
+
+    // constructeur
+    public Task() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getDueAt() {
+        return dueAt;
+    }
+
+    public void setDueAt(LocalDateTime dueAt) {
+        this.dueAt = dueAt;
+    }
+}
---
+++ backend/src/main/java/com/taskito_web/backend/model/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.taskito_web.backend.model;
+
+public enum TaskStatus {
+    TO_DO,
+    IN_PROGRESS,
+    COMPLETED,
+}
