1efd0824b3bdae6c5221ebd29bfc20e7240d58a0
Fredhack1 <mirdo2co@gmail.com>
2025-08-05 15:47:56 +0100

    Adding services and implement the interface v2

--- backend/src/main/java/com/taskito_web/backend/service/TaskService.java
+++ backend/src/main/java/com/taskito_web/backend/service/TaskService.java
@@ -2,28 +2,18 @@ package com.taskito_web.backend.service;
 
 import com.taskito_web.backend.model.Task;
 import com.taskito_web.backend.repository.TaskRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class TaskService {
+public interface TaskService {
 
-    private TaskRepository taskRepository;
+    List<Task> getAllTasks();
 
-    public List<Task> findAll(){
-        return taskRepository.findAll();
-    }
+    Task getTaskById(Long id);
 
-    public Task findById(Long id){
-        return taskRepository.findById(id).orElse(null);
-    }
+    Task createTask(Task task);
 
-    public Task saveOrUpdate(Task task){
-        return taskRepository.save(task);
-    }
+    Task updateTask(Long id, Task updatedTask);
 
-    public void deleteById(Long id){
-        taskRepository.deleteById(id);
-    }
+    void deleteTask(Long id);
 }
--- backend/src/main/java/com/taskito_web/backend/service/TaskServiceImpl.java
+++ backend/src/main/java/com/taskito_web/backend/service/TaskServiceImpl.java
@@ -1,7 +1,38 @@
 package com.taskito_web.backend.service;
 
+import com.taskito_web.backend.model.Task;
 import com.taskito_web.backend.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public abstract class TaskServiceImpl implements TaskRepository {
+import java.util.List;
 
+@Service
+public class TaskServiceImpl {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id).get();
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Long id, Task updatedTask) {
+        return taskRepository.save(updatedTask);
+    }
+
+    public void deleteTaskById(Long id) {
+        taskRepository.deleteById(id);
+    }
 }
