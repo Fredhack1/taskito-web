58f29d5b213cf7cd87279f61e013e3280b715999
Fredhack1 <mirdo2co@gmail.com>
2025-08-06 13:55:02 +0100

    Add CORS support in TaskController and remove unused method in TaskRepository

--- backend/src/main/java/com/taskito_web/backend/repository/TaskRepository.java
+++ backend/src/main/java/com/taskito_web/backend/repository/TaskRepository.java
@@ -4,5 +4,4 @@ import com.taskito_web.backend.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    Long id(Long id);
 }
